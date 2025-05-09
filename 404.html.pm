fe8ca348acd45cdc185be7d367c4ce555930b7ff
Brendon Bown <brendon.bown@gmail.com>
2025-05-09 15:43:22 -0600

    [pollen-refactor] Add 404 page

---
+++ 404.html.pm
@@ -0,0 +1,9 @@
+#lang pollen
+
+◊(define-meta title "Not Found")
+◊(define-meta stylesheets ("404.css"))
+
+◊h1{Not Found}
+
+◊p{◊strong{Oops!} It looks like the page you are looking for does not exist.}
+◊p{It may be a broken link, or the page may have been removed...}
